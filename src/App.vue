4d36673686934c213c184810f2a333fbe4399456
Hubert <hubert.milewski35@gmail.com>
2026-07-27 20:51:51 +0200

    feat: sekcja instagram

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Navbar from '@/components/layout/Navbar.vue'
 import Intro from '@/components/sections/Intro.vue'
 import AboutCompany from '@/components/sections/AboutCompany.vue'
 import Ofert from '@/components/sections/Ofert.vue'
+import InstagramContact from '@/components/sections/InstagramContact.vue'
 </script>
 
 <template>
@@ -10,6 +11,7 @@ import Ofert from '@/components/sections/Ofert.vue'
   <Intro class="pt-18" />
   <Ofert />
   <AboutCompany />
+  <InstagramContact />
 </template>
 
 <style scoped></style>
