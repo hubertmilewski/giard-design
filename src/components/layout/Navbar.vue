4460c3c8f347913347f9272cd2483f4d798e59f0
Hubert <hubert.milewski35@gmail.com>
2026-07-28 18:30:15 +0200

    style: ponowna poprawa animacji wysuwania menu na mobile

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -316,8 +316,8 @@ onBeforeUnmount(() => {
         style="height: calc(100dvh - 73px)"
         :class="
           mobileMenuOpen
-            ? 'translate-y-0 opacity-100 border-t border-base pointer-events-auto'
-            : '-translate-y-3 opacity-0 pointer-events-none'
+            ? 'translate-x-0 opacity-100 pointer-events-auto'
+            : 'translate-x-full opacity-0 pointer-events-none'
         "
       >
         <div class="flex flex-col min-h-full py-8 container-custom">
